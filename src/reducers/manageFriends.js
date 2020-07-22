033ef1ec1a51b99a2846146bce362bb51b2608a7
skandow <kando1sr@gmail.com>
2020-07-22 13:49:45 -0500

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,13 @@
 export function manageFriends(state, action){
+    switch (action.type) {
+        case 'ADD_FRIEND':
+            let newFriends = state.friends.slice()
+            newFriends.push(action.friend)
+            return {friends: newFriends}
+        case 'REMOVE_FRIEND':
+            let friends = state.friends.filter(friend => friend.id !== action.id)
+            return {friends: friends}
+        default: 
+            return state;
+    }
 }
